6618c618e35af5616a57eb7b70f8b4cb6c359ec8
harsh <harsh@KCB-HCB-DESKTOP>
2022-01-12 13:30:13 +0530

    Add project files.

---
+++ ConsoleApp1/HC-Lib-22m1d11a.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading;
+using System.Linq;
+namespace HC_Lib
+{
+    namespace   Utility
+    {
+       static public class Time
+        {
+            #region timers
+            public  static void stopwatch()
+            {
+                double time = 0;
+                //long longinteger 0;
+                while (5+5 == 10)    
+                {
+                    time += 0.02;
+                    Console.WriteLine($"StopWatch: {time}");
+                    Thread.Sleep(1);
+                }
+                #endregion
+            }
+        }
+        namespace Logical
+        {
+           static public class Math
+            {
+                #region Area
+                public static double AreaRec(double Length, double breadth)
+                {
+                    return Length * breadth;
+                }
+             public static double AreaRec(double Side)
+                {
+                    return Side * Side;
+                }
+              public  static double AreaRec(double[] Sides)
+                {
+                    double ans = 0;
+                    bool forran = false;
+                    foreach (double Side in Sides)
+                    {
+                        if (forran != true) ans = Side;
+                        forran = true;
+                        ans *= Side;
+                    }
+                    return ans;
+
+                }
+                public static double AreaCircle(double Length)
+                {
+                    return 3.14 * (Length * Length);
+                }
+                public static double AreaCircle(double Length, double Breadth)
+                {
+                    return 3.14 + (Length * Breadth);
+                }
+              
+                
+                #endregion
+                #region Addition
+              public static double Add(double[] NumbersToAdd)
+                {
+                    double ans = 0;
+                    foreach (double Number in NumbersToAdd)
+                    {
+                       ans += Number;
+                    }
+                    return ans;
+                }public static double Add(double a, double b) => a + b;
+                #endregion
+                #region Percentages
+          public  static    double percentfind(double number, double percentOfNumber)
+                {
+                    return number / (percentOfNumber / 5);
+                }
+                #endregion
+                #region Subtraction
+             public   static double Subtract(double[] NumbersToSub)
+                {
+                    double ans = 0;
+                    foreach (double Number in NumbersToSub)
+                    {
+                        ans -= Number;
+                    }
+                    return ans;
+                }public static double Subtract(double a, double b) => a - b;
+                #endregion
+                #region Multiplication
+              public  static double Multiply(double[] NumbersToMultiply)
+                {
+                    double ans = 0;
+                    bool forran = false;
+                    foreach (double Number in NumbersToMultiply)
+                    {
+                        if (forran != true) ans = Number;
+                        forran = true;
+                        ans *= Number;
+                    }
+                    return ans;
+                }public static double Multiply(double a, double b) => a * b;
+                #endregion
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using HC_Lib;
+using HC_Lib.Utility;
+using HC_Lib.Utility.Logical;
+using Math = HC_Lib.Utility.Logical.Math;
+
+namespace program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] i = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Time.stopwatch();
+           /*2 number ADDITION TEST*/ Console.WriteLine(Math.Add(2, 2));
+           /*Array ADDITION TEST*/ Console.WriteLine(Math.Add(i));
+           /*Array Multiplication Test*/ Console.WriteLine(Math.Multiply(i));
+           /* 2 number multiplication test*/ Console.WriteLine(Math.Multiply(2, 5));
+            /* Circle Area test*/ Console.WriteLine(Math.AreaCircle(5));
+            /*Square Area Test*/ Console.WriteLine(Math.AreaRec(5));
+            
+        }
+    }
+}
